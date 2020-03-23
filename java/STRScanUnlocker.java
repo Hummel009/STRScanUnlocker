0e5878c588a85e081727928c65d4ccbca37afb48
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2020-03-23 16:17:25 +0300

    Add files via upload

---
+++ java/STRScanUnlocker.java
@@ -0,0 +1,12 @@
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLServerStartingEvent;
+import lotr.common.command.LOTRCommandStrScan;
+import net.minecraft.command.ICommand;
+
+@Mod(modid="strscan", dependencies="required-after:lotr")
+public class STRScanUnlocker {
+    @Mod.EventHandler
+    public void serverStarting(FMLServerStartingEvent e) {
+        e.registerServerCommand((ICommand)new LOTRCommandStrScan());
+    }
+}
