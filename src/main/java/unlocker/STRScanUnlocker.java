40d22ef9758a4e9928a690084bf3afbd6e343dcc
Hummel009 <emperorbur@gmail.com>
2022-07-28 15:45:07 +0300

    refactoring

--- src/main/java/unlocker/STRScanUnlocker.java
+++ src/main/java/unlocker/STRScanUnlocker.java
@@ -1,9 +1,11 @@
+package unlocker;
+
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
 import lotr.common.command.LOTRCommandStrScan;
 import net.minecraft.command.ICommand;
 
-@Mod(modid="strscan", dependencies="required-after:lotr")
+@Mod(modid="unlocker", dependencies="required-after:lotr")
 public class STRScanUnlocker {
     @Mod.EventHandler
     public void serverStarting(FMLServerStartingEvent e) {
