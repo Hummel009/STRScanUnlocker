7ec1338e645e133b3843c8cd19974c220e9d88fa
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-02-07 14:11:35 +0300

    c

--- src/main/java/com/github/hummel/strscan/Main.java
+++ src/main/java/com/github/hummel/strscan/Main.java
@@ -1,11 +1,11 @@
-package unlocker;
+package com.github.hummel.strscan;
 
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
 import lotr.common.command.LOTRCommandStrScan;
 
-@Mod(modid = "unlocker", dependencies = "required-after:lotr", useMetadata = true)
-public class STRScanUnlocker {
+@Mod(modid = "strscan", dependencies = "required-after:lotr", useMetadata = true)
+public class Main {
 	@Mod.EventHandler
 	public void serverStarting(FMLServerStartingEvent e) {
 		e.registerServerCommand(new LOTRCommandStrScan());
